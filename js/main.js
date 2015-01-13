18be2fcfffe69bc900ff2c33c717a7d0ca73ab66
pocke <p.ck.t22@gmail.com>
2015-01-13 20:55:30 +0900

    works

---
+++ js/main.js
@@ -0,0 +1,33 @@
+(function () {
+
+  Vue.component('works-item', {
+    template: '#works-item-template',
+  });
+
+  var app = new Vue({
+    el: '#vue-main',
+    data: {
+      works: {
+        applications: [
+          {
+            title:  'Vim Scouter Web',
+            url:    'https://github.com/pocke/vim_scouter_web',
+            detail: 'Vim戦闘力をWebから計測',
+          },
+          {
+            title:  'slコマンドが走りました',
+            url:    'https://github.com/pocke/sl_tweet',
+            detail: 'slコマンドが走る度にTwitterに呟く。',
+          },
+        ],
+        'mikutter plugins': [
+          {
+            title:  'mikutter vimize',
+            url:    'https://github.com/pocke/mikutter_vimize',
+            detail: 'mikutter でVimライクなキーバインドを実現するプラグイン',
+          }
+        ],
+      },
+    },
+  });
+})();
